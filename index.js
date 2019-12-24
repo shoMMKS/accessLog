8ff21be4ed45f6638980bc7a27df9c65c7f909dd
shoMMKS <sho15-kanan-town-1739@ezweb.ne.jp>
2019-12-24 18:20:04 +0900

    question 2

--- index.js
+++ index.js
@@ -16,20 +16,22 @@ const monthObject = {
   'Dec': 11,
 }
 
-const displayFile = async (file) => {
-  const stream = fs.createReadStream(file, 'utf8')
-  const reader = readline.createInterface({ input: stream })
+const displayFile = async () => {
   const hostObject = {}
   const hoursObject = {}
+  for (let i = 2; i < process.argv.length; i++) {
+    const stream = fs.createReadStream(process.argv[i], 'utf8')
+    const reader = readline.createInterface({ input: stream })
 
-  for await (const data of reader) {
-    const array = data.split(' ')
-    const host = array[0]
-    const dateArray = array[3].replace('[', '').split(/\/|:/)
-    const time = new Date(dateArray[2], monthObject[dateArray[1]], dateArray[0], dateArray[3], dateArray[4], dateArray[5])
-    const hour = time.getHours()
-    hostObject[host] = hostObject[host] ? hostObject[host] + 1 : 1
-    hoursObject[hour] = hoursObject[hour] ? hoursObject[hour] + 1 : 1
+    for await (const data of reader) {
+      const array = data.split(' ')
+      const host = array[0]
+      const dateArray = array[3].replace('[', '').split(/\/|:/)
+      const time = new Date(dateArray[2], monthObject[dateArray[1]], dateArray[0], dateArray[3], dateArray[4], dateArray[5])
+      const hour = time.getHours()
+      hostObject[host] = hostObject[host] ? hostObject[host] + 1 : 1
+      hoursObject[hour] = hoursObject[hour] ? hoursObject[hour] + 1 : 1
+    }
   }
   console.log('- リモートホスト別のアクセス件数 -')
   Object.keys(hostObject).sort((a, b) => hostObject[b] - hostObject[a]).map(key => {
@@ -41,4 +43,9 @@ const displayFile = async (file) => {
   })
 }
 
-displayFile('./sample_log')
+if (process.argv.length > 2) {
+  displayFile()
+} else {
+  console.log('Usage: node index.js [fileNames]')
+  console.log('Example: node index.js sample1.txt sample2.txt sample3.txt')
+}
---
+++ lab.js
@@ -0,0 +1,2 @@
+console.log(args[0])
+console.log(args[1])
